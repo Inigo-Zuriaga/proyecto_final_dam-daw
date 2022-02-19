22f46e3cc0b183aafcd4dcf670309805fe0df02f
ZuRi-To <91980222+ZuRi-To@users.noreply.github.com>
2022-02-19 15:09:29 +0100

    TARJETAS DE EQUIPO

--- resources/views/app/equipo.blade.php
+++ resources/views/app/equipo.blade.php
@@ -2,87 +2,83 @@
 
 @section('content')
 
-<div style="text-align: center">
-<h1>DAW</h1>
-    <div class="row" style="">
-        <div class="col s12 m3">
+
+    <div class="row">
+
+        <div class="col s12 m4 l2"><p style="background-color: #66A523">s12 m4</p></div>
+        <div class="col s12 m4 l2"><p style="background-color: #ff8d0e">s12 m4</p></div>
+        <div class="col s12 m4 l2"><p style="background-color: red">s12 m4</p></div>
+    </div>
+    <div class="row">
+        <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
+        <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
+        <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
+        <div class="col s12 m6 l3"><p>s12 m6 l3</p></div>
+    </div>
+
+    <div class="row" style="text-align: center">
+        <h1>DAW</h1>
+        <div class="col s12 m3 l3" style="margin-left: 10%" >
             <div class="card" style="">
                 <div class="card-image">
-                    <img src="http://3.143.248.187/proyecto_final_dam-daw/public/img/crashbandicoot1.jpg" style="width: 80%">
-                    <br><br>
-                    <span class="card-title">Iñigo Zuriaga</span>
-
+                    <img src="http://3.143.248.187/proyecto_final_dam-daw/public/img/crashbandicoot1.jpg" style="width: 80%; margin-left: 40px">
                 </div>
                 <div class="card-content">
-                    <p>DAW. Hola que tal</p>
+                    <p>Pablo Horcajada</p>
                 </div>
             </div>
         </div>
 
-        <div class="col s12 m3">
-            <div class="card">
+        <div class="col s12 m3 l3" >
+            <div class="card" style="">
                 <div class="card-image">
-                    <img src="http://3.143.248.187/proyecto_final_dam-daw/public/img/crashbandicoot1.jpg" style="width: 80%">
-                    <br><br>
-                    <span class="card-title">Iñigo Zuriaga</span>
-
+                    <img src="http://3.143.248.187/proyecto_final_dam-daw/public/img/crashbandicoot1.jpg" style="width: 80%; margin-left: 40px">
                 </div>
                 <div class="card-content">
-                    <p>DAW. Hola que tal</p>
+                    <p>Iñigo Zuriaga</p>
                 </div>
             </div>
         </div>
 
-            <div class="col s12 m3">
-            <div class="card">
+        <div class="col s12 m3 l3" >
+            <div class="card" style="">
                 <div class="card-image">
-                    <img src="http://3.143.248.187/proyecto_final_dam-daw/public/img/crashbandicoot1.jpg" style="width: 80%">
-                    <br><br>
-                    <span class="card-title">Iñigo Zuriaga</span>
-
+                    <img src="http://3.143.248.187/proyecto_final_dam-daw/public/img/crashbandicoot1.jpg" style="width: 80%; margin-left: 40px">
                 </div>
                 <div class="card-content">
-                    <p>DAW. Hola que tal</p>
+                    <p>Alex gfdggfdgf</p>
                 </div>
             </div>
         </div>
 
     </div>
-<h1></h1>
-<h1>DAM</h1>
-    <div class="row">
-        <div class="col s12 m3">
+
+    <div class="row" style="text-align: center">
+        <h1>DAM</h1>
+        <div class="col s12 m3 l3" style="margin-left: 25%" >
             <div class="card" >
                 <div class="card-image">
-                    <img src="http://3.143.248.187/proyecto_final_dam-daw/public/img/crashbandicoot1.jpg" style="width: 80%">
-                    <br><br>
-                    <span class="card-title">Iñigo Zuriaga</span>
-
+                    <img src="http://3.143.248.187/proyecto_final_dam-daw/public/img/crashbandicoot1.jpg" style="width: 80%;margin-left: 40px">
                 </div>
                 <div class="card-content">
-                    <p>DAW. Hola que tal</p>
+                    <p>Max gfdgdfg</p>
                 </div>
             </div>
         </div>
 
-        <div class="col s12 m3">
-            <div class="card">
+        <div class="col s12 m3 l3" >
+            <div class="card" style="">
                 <div class="card-image">
-                    <img src="http://3.143.248.187/proyecto_final_dam-daw/public/img/crashbandicoot1.jpg" style="width: 80%">
-                    <br><br>
-                    <span class="card-title">Iñigo Zuriaga</span>
-
+                    <img src="http://3.143.248.187/proyecto_final_dam-daw/public/img/crashbandicoot1.jpg" style="width: 80%; margin-left: 40px">
                 </div>
                 <div class="card-content">
-                    <p>DAW. Hola que tal</p>
+                    <p>Yago gfdggfdgf</p>
                 </div>
             </div>
-        </div>
-
         </div>
 
     </div>
-</div>
+
 
 
 @endsection
