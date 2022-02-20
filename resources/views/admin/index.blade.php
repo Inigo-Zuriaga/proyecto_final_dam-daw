96b663f0ac27b0a8673c72a0b6412dc60e4d108b
sporovich <phgpablito@gmail.com>
2022-02-20 13:09:23 +0100

    cambio vista de perfil

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,21 +6,30 @@
         <h2>{{Auth::user()->usuario}}</h2>
         {{ Html::image('img/'.$row->imagen, 'foto perfil', ['class'=>'circle']) }}
         <div class="card-content">
-            <p><span>-Usuario: </span>{{ $row->usuario }}</p>
-            <p><span>-Fecha creación cuneta: </span>{{ $row->created_at }}</p>
-            <p><span>-Email: </span>{{ $row->email }}</p>
+            <div class="infotitulo">Información básica</div>
+            <div class="info">
+                <p><span>Nombre </span>{{ $row->usuario }}</p>
+                <p><span>Correo electrónico </span>{{ $row->email }}</p>
+                <p><span>Contraseña </span>******</p>
+                <p><span>Fecha creación cuenta </span>{{ $row->created_at }}</p>
+
+                <p class="bio">{{ $row->biografia }}</p>
+            </div>
         </div>
-        <p class="bio">{{ $row->biografia }}</p>
+
         <div class="card-content">
             <div class="card-action">
-                <a href="#">Editar</a>
+                <!--
                 <span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
             </div>
         </div>
+
+
         <div class="card-reveal" style="background-color: black;">
-            <span class="card-title  text-darken-4"><span> {{ $row->usuario }}</span><i class="material-icons right">close</i></span>
-            <span> {{ $row->biografia }}</span>
-        </div>
+            <span class="card-title  text-darken-4"><span>  $row->usuario </span><i class="material-icons right">close</i></span>
+            <span>  $row->biografia </span>
+        </div> -->
+            </div>
     </div>
 <form method="POST" action="">
     @csrf
